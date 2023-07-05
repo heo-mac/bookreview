f4f11678f871aa1df869b5bb9957a733379086c5
heo-mac <skmn0105@gmail.com>
2023-07-06 00:24:11 +0900

    go-router

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,10 +1,41 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
 
   @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  late GoRouter router;
+
+  @override
+  void initState() {
+    super.initState();
+    router = GoRouter(routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) => const HomePage(),
+      ),
+      GoRoute(
+        path: '/detail',
+        builder: (context, state) => const DetailPage(),
+      ),
+    ], initialLocation: '/');
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp.router(
+      routerConfig: router,
+      theme: ThemeData(
+          appBarTheme: const AppBarTheme(
+              elevation: 0,
+              backgroundColor: Color(0xff1c1c1c),
+              titleTextStyle: TextStyle(color: Colors.white)),
+          scaffoldBackgroundColor: const Color(0xff1c1c1c)),
+    );
   }
 }
